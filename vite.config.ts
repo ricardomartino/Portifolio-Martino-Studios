b0bf5b45b66700496e44f60c19a02c9856f7982e
Ricardo Martins <ric.martinodev@gmail.com>
2026-03-06 11:34:07 -0300

    primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Portifolio-Martino-Studios/',
+    base: '/Martino-Studios/',
     plugins: [react(), tailwindcss()],
     build: {
       outDir: 'docs',
